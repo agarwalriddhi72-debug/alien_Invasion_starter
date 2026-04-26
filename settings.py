71c0d92ec798a93dac62ca49ea990e2bc081d2c6
Riddhi <agarwal.riddhi72@gmail.com>
2026-04-26 01:25:42 -0400

    p3 - fleshed out game stats to update the hi score and save and read from a file

--- settings.py
+++ settings.py
@@ -10,6 +10,7 @@ class Settings:
         self.FPS: int = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
         self.difficulty_scale = 1.1
+        self.scores_file = Path.cwd() / 'Assets' / 'file' / 'scores.json'
 
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
         self.ship_w = 40
