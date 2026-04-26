cc146e48bf69259409f3410150da2c09bcb34529
Riddhi <agarwal.riddhi72@gmail.com>
2026-04-26 00:20:30 -0400

    fixed spelling bug

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings:
         self.screen_h: int = 500
         self.FPS: int = 60
         self.bg_file = Path.cwd() / 'Assets' / 'images' / 'Starbasesnow.png'
-        self.diffuculty_scale = 1.1
+        self.difficulty_scale = 1.1
 
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
         self.ship_w = 40
